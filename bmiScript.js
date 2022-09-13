f89ec10b6ff153aef17c7cbff08e9684deff6308
Muhammad Jawwad <98478303+Muhammad-Jawwad@users.noreply.github.com>
2022-09-13 18:14:47 +0500

    Update bmiScript.js

--- bmiScript.js
+++ bmiScript.js
@@ -1,75 +1,18 @@
-class bmiCal{
-    constructor(weightValue,weightUnit,heightFeets,heightInches){
-        this.weightValue = weightValue;
-        this.weightUnit = weightUnit;
-        this.heightFeets = heightFeets;
-        this.heightInches = heightInches;
-        this.clear();
-    }
-    clear(){
-        this.weight = 0;
-        this.height = 0;
-        this.bmivalue = '';
-    }
-
-    massInKg(mass,unit){
-        if(unit!="Pounds" && unit!="Kilograms") return alert("Unit of weight is not defined");
-        else if(unit=="Pound"){
-            if(mass<=100) return alert("Weight should be more than 100 pounds")
-            this.weight = (mass / 10);
-        }
-        else{
-            if(mass<=10) return alert("Weight should be more than 10 kg")
-            this.weight = mass;
-        }
-    }
-
-    heightInMeter(feets,inches){
-        
-    }
-
-    computeBMI(){
-        // BMI=kg/m^2
-        let bmi ;
-        if (isNaN(this.weight) || isNaN(this.height)) return 
-        else{
-            bmi = this.weight / (this.height**2);
-        }
-        this.bmivalue = bmi;
-
-    }
-
-    displayBMI(){
-        this.displayResult.innerText = "Your BMI is:";
-
-    }
+function getBMI(){
+    let weightInput = document.getElementById(['weight']).value;
+    let heightInput = document.getElementById(['height']).value;
     
+    heightInput = heightInput * 12; //converting it into inches
+    heightInput = heightInput * 0.025;
 
+    let bmiValue = weightInput/Math.pow(heightInput,2);
+    bmiValue = Math.round(bmiValue);
+
+    document.getElementById('bmi-value').innerText = bmiValue;
 }
-
-//Including DOM from html through ID
-const calButton = document.querySelector(['data-calculate']);
-const clearButton = document.querySelector(['data-clear']);
-const weightValue = document.querySelector(['data-weight']);
-const weightUnit = document.querySelector(['data-weight-unit']);
-const heightFeets = document.querySelector(['data-height-feet']);
-const heightInches = document.querySelector(['data-height-inch']);
-const displayResult = document.querySelector(['display-result']);
-
-//Making an object calculator of class Calculator... 
-const bmical = new bmiCal(weightValue,weightUnit,heightFeets,heightInches);
-
-//Now telling buttons what to do on click
-calButton.addEventListener('click', (wg,opt1,opt2,opt3) => {
-    bmical.massInKg(wg.innerText,opt1.innerText);
-    bmical.heightInMeter(opt2.innerText,opt3.innerText);
-    bmical.computeBMI();
-    bmical.displayBMI();
-})
-clearButton.addEventListener('click', button => {
-    bmical.clear()
-    bmical.displayBMI()
-})
-
-
-
+function clearData(){
+    console.log("hey");
+    document.getElementById(['weight']).value = '';
+    document.getElementById(['height']).value = '';
+    document.getElementById('bmi-value').innerText = '';
+}   
